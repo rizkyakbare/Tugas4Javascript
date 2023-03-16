9631362c641b34440afb737f04ac8eb9dc23405b
rizkyakbare <rizky.akbar.ramadhan-2020@gmail.com>
2023-03-16 12:29:20 +0700

    commit2

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
   
-    if (username == "mimin" && password == "mimin123") {
-      alert("Selamat Datang Mimin, Anda Administrator");
-    } else if (username == "budi" && password == "budi123") {
-      alert("Selamat Datang Budi, Anda Operator");
+    if (username == "antok" && password == "antok123") {
+      alert("Selamat Datang Antok, Anda Admin");
+    } else if (username == "totok" && password == "totok123") {
+      alert("Selamat Datang totok, Anda Supervisor");
     } else {
       alert("Maaf Anda Gagal Login");
     }
